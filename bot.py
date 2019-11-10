d0773f785d733afc7b9724a79b8f70de0743eed8
JosephJvB <joevanbo@pm.me>
2019-11-10 14:49:43 +1300

    refactor(bot): rename vars, create respond method

--- bot.py
+++ bot.py
@@ -8,11 +8,12 @@ class Bot:
     def __init__(self):
         print('*****\nbot up\n*****')
         self.msg_payload = None
-        self.guesser_id = None
-        self.whomst_id = None
+        self.msg_user_id = None
+        self.guess_user_id = None
+        self.actual_user_id = None
         self.track = None
 
-    # Need to look up what pythons switch statements are like, could be the buzz!!
+    # get guesserId, guesseeId, and save payload to react to initial message
     def handle_whom_cmd(self, payload):
         data = payload['data']
         text = data['text']
@@ -22,50 +23,46 @@ class Bot:
 
         if not self.msg_payload and len(mentions) > 0:
             self.msg_payload = payload
-            self.guesser_id = user
-            self.whomst_id = mentions[0]
+            self.msg_user_id = user
+            self.guess_user_id = mentions[0]
             print(f'guess received: user={user}, text={text}')
             # react with speech bubble: todo
             Timer(5, self.reset).start() # reset after delay
-
         return
 
+    # if bot msg is /whom response, set actual_user_id and track
     def handle_bot_message(self, payload):
-        web_client = payload['web_client']
         data = payload['data']
-        channel_id = data['channel']
         text = data['text']
-        msg_text = ''
 
-        if not self.guesser_id or not self.whomst_id or not text.startswith('This track,'):
-            # cases: bot didnt respond in time OR track added in spotify direct
-            # remove pending speechbubble reaction from initial message using self.msg_payload
-            return
-
-        whomst = text.split('<@')[1].replace('>', '')
-        if whomst == self.whomst_id:
+        if self.msg_user_id and self.guess_user_id and text.startswith('This track,'):
+            self.actual_user_id = text.split('<@')[1].replace('>', '')
             self.track = text.split('This track,')[1].split(', was last requested')[0]
-            msg_text = 'Correct!'
-            # do database operation to save success
-        else:
-            msg_text ='Bad guess!'
+            self.respond()
+        return
 
-        # if success, react check, else react cross
-        # web_client.reactions_add(
-        #     channel=channel_id,
-        #     name='thumbsup',
-        # )
-        web_client.chat_postMessage(
-            channel=channel_id,
-            text=msg_text,
-        )
+    def respond(self):
+        if self.msg_payload and self.msg_user_id and self.guess_user_id and self.actual_user_id and self.track:
+            success = self.guess_user_id == self.actual_user_id
+            # todo: randomly choose response from a set
+            if success:
+                text = 'Correct!'
+                reaction = ':white_check_mark:'
+            else:
+                text = 'Better luck next time!'
+                reaction = ':x:'
+
+            web_client = self.msg_payload['web_client']
+            web_client.chat_postMessage(channel=os.environ['CHANNEL_ID'], text=text)
+            web_client.reactions_add(name=reaction)
         self.reset()
         return
 
     def reset(self):
         print('\nRESETTING\n')
         self.msg_payload = None
-        self.whomst_id = None
-        self.guesser_id = None
+        self.msg_user_id = None
+        self.guess_user_id = None
+        self.actual_user_id = None
         self.track = None
 
