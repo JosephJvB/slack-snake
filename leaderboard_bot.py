1240420647eaf74391ffe6f453ca3b5b92fbbf34
JosephJvB <joevanbo@pm.me>
2019-11-18 13:52:43 +1300

    leaderboard looks at redis

--- leaderboard_bot.py
+++ leaderboard_bot.py
@@ -1,40 +1,27 @@
-import random
-from threading import Timer
-from db import Records
-from peewee import fn
+import os
+import redis
 
 from base_bot import Base_Bot
-
-# emoji_dict = {
-#     0: ':one:',
-#     1: ':two:',
-#     2: ':three:',
-#     3: ':four:',
-#     4: ':five:',
-#     5: ':six:',
-#     6: ':seven:',
-#     7: ':eight:',
-#     8: ':nine:',
-#     9: ':keycap_ten:',
-# }
 
 class Leaderboard_Bot(Base_Bot):
     def __init__(self):
         super(Leaderboard_Bot, self).__init__()
+        redis_url = os.getenv('REDIS_URL')
+        if redis_url:
+            self.redis = redis.Redis.from_url(redis_url)
+        else:
+            raise Exception('Redis env vars missing')
 
     def handle_leaderboard_cmd(self, p):
         self.msg_payload = p
         self.add_react('speech_balloon')
-        query_results = (Records.select(Records.user, fn.COUNT('*')
-            .alias('count'))
-            .group_by(Records.user)
-            .order_by(fn.COUNT('*').alias('count').desc()))
-        if len(query_results) == 0:
+        lb = self.get_leaderboard()
+        if len(lb) == 0:
             text = 'Nobody has any points yet!'
         else:
             text = '*Leaderboard:*\n'
-            for i, r in enumerate(query_results[:10]):
-                text += f'*{i + 1}* {r.user}: *{r.count}*\n'
+            for i, r in enumerate(lb):
+                text += f'*{i + 1}.* {r.name}: *{r.points}*\n'
         self.remove_react('speech_balloon')
         self.post_msg(text)
         return
@@ -43,11 +30,22 @@ class Leaderboard_Bot(Base_Bot):
         self.msg_payload = p
         self.add_react('speech_balloon')
         name = self.get_user_name(p['data']['user'])
-        query_results = (Records.select(Records.user, fn.COUNT('*')
-            .alias('count'))
-            .where(Records.user==name))
-        p = query_results[0].count
+        p = int(self.redis.get(name))
         text = f'*{name}* is on *{p}* '
         text += 'point!' if p == 1 else 'points!'
         self.remove_react('speech_balloon')
         self.post_msg(text)
+
+    def get_leaderboard(self):
+        keys = self.redis.keys()
+        vals = self.redis.mget(keys)
+        scores = []
+        for i, k in enumerate(keys):
+            scores.append({
+                'name': k,
+                'points': int(vals[i])
+            })
+        # sort by largest points value > smallest
+        scores.sort(key=lambda i: i['points'],
+        reverse=True)
+        return scores
--- test.py
+++ test.py
@@ -1,3 +1,7 @@
-from db import Records
-
-Records.create(user='joe', track='the sound of silence')
+a = [
+    {'x': 1},
+    {'x': 2},
+    {'x': 3},
+]
+a.sort(reverse=True, key=lambda i: i['x'])
+print(a)
