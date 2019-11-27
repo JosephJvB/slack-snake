1cc16a6bccf849676d157023c2a2ee23a3cee510
JosephJvB <joevanbo@pm.me>
2019-11-27 23:30:47 +1300

    wip

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import slack
 from whom_bot import Whom_Bot
 from leaderboard_bot import Leaderboard_Bot
+from banger_bot import Banger_Bot
 
 token = os.environ['BOT_TOKEN']
 
@@ -11,31 +12,35 @@ if not token:
 else:
     whom_bot = Whom_Bot()
     leaderboard_bot = Leaderboard_Bot()
+    banger_bot = Banger_Bot()
+
+    CMDS = {
+        os.environ['POINTS_CMD']: leaderboard_bot.handle_points_cmd,
+        os.environ['WHOM_CMD']: whom_bot.handle_whom_cmd,
+        os.environ['LB_CMD']: leaderboard_bot.handle_leaderboard_cmd,
+    }
 
     @slack.RTMClient.run_on(event='message')
     def on_message(**payload):
         data = payload['data']
         print('@ on_message', data)
 
+        if data.get('bot_profile'):
+            if data['channel'] == os.environ['CHANNEL_ID']:
+                t = payload['data']['text']
+                whom_bot.handle_bot_message(t)
+            if data['channel'] == os.environ['BOT_DM']:
+                banger_bot.handle_banger_cmd(payload)
+            return
+
         if not data.get('text') or data['channel'] != os.environ['CHANNEL_ID']:
             return
 
-        if data['text'] == os.environ['POINTS_CMD']:
-            leaderboard_bot.handle_points_cmd(payload)
-            return
 
-        if data.get('bot_profile'):
-            t = payload['data']['text']
-            whom_bot.handle_bot_message(t)
-            return
-
-        if data['text'].startswith(os.environ['WHOM_CMD']):
-            whom_bot.handle_whom_cmd(payload)
-            return
-
-        if data['text'].startswith(os.environ['LB_CMD']):
-            leaderboard_bot.handle_leaderboard_cmd(payload)
-            return
+        cmd = data['text'].split(' ')[0]
+        handler = CMDS.get(cmd)
+        if handler:
+            handler(payload)
 
         return
 
--- readme.md
+++ readme.md
@@ -1,5 +1,11 @@
 # Me make snake robot for slack
 
+### todo
+- !banger : gives points to the person who had requested the song
+	- Ah problem...I cant query Jukebots db
+	- !banger, my bot send command to Jukebot in DM and uses response to allocate point
+	- or: !banger, my bot send /whom in playlist and allocate point - ending whom round
+
 ### TIL:
  - https://github.com/os/slacker: I can have my bot send commands with `chat.command`
     - user types !whom @user
