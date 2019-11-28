8cf1437ba2e00c080f9f60fc714ec86d10dd7d69
JosephJvB <joevanbo@pm.me>
2019-11-28 22:41:20 +1300

    bugfix

--- redis_migration.py
+++ redis_migration.py
@@ -13,7 +13,7 @@ else:
     keys = redis.keys()
     for key in keys:
         k = key.decode('utf8')
-        if not k.startswith('user:') and not key.startswith('song:'):
+        if not k.startswith('user:') and not k.startswith('song:'):
             v = redis.get(k)
             redis.set('user:' + k, v)
             redis.delete(k)
